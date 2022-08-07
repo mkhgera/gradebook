7e9e7914df0a24e5b7aa36ecd7d636ca5d696d05
Aigerim Mukhamadiyeva <mkhgera@outlook.com>
2022-08-07 20:30:18 +0600

    Demo

---
+++ src/GradeBook/Book.cs
@@ -0,0 +1,75 @@
+namespace GradeBook
+{
+    public delegate void GradeAddedDelegate(Object sender, EventArgs args);
+
+    public class Book : NamedObject
+    {
+        public Book(string name) : base(name)
+        {
+            grades = new List<double>();
+            Name = name;
+        }
+
+        public void AddGrade(double grade)
+        {
+            if(grade <= 100 && grade >= 0)
+            {
+                grades.Add(grade);
+                if(GradeAdded != null)
+                {
+                    GradeAdded(this, new EventArgs());
+                }
+            }
+            else
+            {
+                throw new ArgumentException($"Inavlid {nameof(grade)}");
+            } 
+            
+        }
+
+        public event GradeAddedDelegate GradeAdded;
+
+        public Statistics GetStatistics()
+        {
+            var result = new Statistics();
+            result.Average = 0.0;
+            result.High = double.MinValue;
+            result.Low = double.MaxValue;
+
+            for(int index = 0; index < grades.Count; index ++)     
+            {
+                result.High = Math.Max(grades[index], result.High);
+                result.Low = Math.Min(grades[index], result.Low);
+                result.Average += grades[index];
+            }
+
+            result.Average /= grades.Count;
+
+            switch(result.Average)
+            {
+                case var d when d >= 90.0: 
+                    result.letter = 'A';
+                    break;
+                case var d when d >= 80.0: 
+                    result.letter = 'B';
+                    break;
+                case var d when d >= 70.0: 
+                    result.letter = 'C';
+                    break;
+                case var d when d >= 60.0: 
+                    result.letter = 'D';
+                    break;
+                default:
+                    result.letter = 'F';
+                    break;
+            }
+
+            return result;
+        }
+
+
+        private List<double> grades;
+
+        public const string CATEGORY = "Scinece";        
+    }
+}
---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace GradeBook
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var book = new Book("August Grade Book");
+            book.GradeAdded += OnGradeAdded;
+            EnterGrade(book);
+
+            var stats = book.GetStatistics();
+
+            System.Console.WriteLine(Book.CATEGORY);
+            System.Console.WriteLine($"For the book named {book.Name}");
+            Console.WriteLine($"The average grade is {stats.Average:N1}");
+            Console.WriteLine($"The highest grade is {stats.High:N1}");
+            Console.WriteLine($"The lowest grade is {stats.Low:N1}");
+            Console.WriteLine($"The letter grade is {stats.letter:N1}");
+        }
+
+        private static void EnterGrade(Book book)
+        {
+            while (true)
+            {
+                System.Console.WriteLine("Enter a grade or q to quit");
+                var input = Console.ReadLine();
+
+                if (input == "q")
+                {
+                    break;
+                }
+
+                try
+                {
+                    var grade = double.Parse(input);
+                    book.AddGrade(grade);
+
+                }
+                catch (ArgumentException ex)
+                {
+                    System.Console.WriteLine(ex.Message);
+                }
+                catch (FormatException ex)
+                {
+                    System.Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    System.Console.WriteLine("**");
+                }
+
+            }
+        }
+
+        static void OnGradeAdded(object sender, EventArgs e)
+        {
+            System.Console.WriteLine("A grade was added");
+        }
+    }
+}
